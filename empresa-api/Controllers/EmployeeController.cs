e5f5fc65f0fb4304ad84ffaacb90970f3b75aa61
Walter Mach <owaldovelasz@gmail.com>
2023-02-10 08:11:39 -0600

    chore: api project set up

---
+++ empresa-api/Controllers/EmployeeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace empresa_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        
+    }
+}
