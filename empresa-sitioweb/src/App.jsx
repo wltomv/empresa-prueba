be2f3c753f84cf217947d570f2542d29bdc8ee40
Walter Mach <owaldovelasz@gmail.com>
2023-02-12 09:20:18 -0600

    feat(website): datatable component

--- empresa-sitioweb/src/App.jsx
+++ empresa-sitioweb/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import CustomDatatable from "./components/CustomDatatable/CustomDatatable";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
+			<CustomDatatable />
 		</div>
 	);
 }
---
+++ empresa-sitioweb/src/components/CustomDatatable/CustomDatatable.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+import { getEmployees } from "../../api";
+import DataTable from "react-data-table-component";
+
+function CustomDatatable() {
+	const [employees, setEmployees] = useState([]);
+
+	const fetchEmployees = async () => {
+		const data = await getEmployees();
+		console.log(data);
+		setEmployees(data);
+	};
+	useEffect(() => {
+		fetchEmployees();
+	}, []);
+
+	const columns = [
+		{
+			name: "DPI",
+			selector: (row) => row.dpi,
+			sortable: true,
+			style: {
+				"&:hover": {
+					cursor: "pointer",
+				},
+			},
+		},
+		{
+			name: "Nombre",
+			selector: (row) => row.fullName,
+			sortable: true,
+			style: {
+				"&:hover": {
+					cursor: "pointer",
+				},
+			},
+		},
+		{
+			name: "Acciones",
+			cell: (row) => {
+				return (
+					<>
+						<button>Editar</button>
+						<button>Eliminar</button>
+					</>
+				);
+			},
+		},
+	];
+
+	const paginationOptions = {
+		rowsPerPageText: "Filas por página",
+		rangeSeparatorText: "de",
+		selectAllRowsItem: true,
+		selectAllRowsItemText: "todos",
+	};
+
+	const customStyles = {
+		rows: {
+			style: {
+				"&:hover": {
+					color: "white",
+					background: "#52658f",
+				},
+			},
+		},
+		headCells: {
+			style: {
+				color: "white",
+				background: "#333a56",
+			},
+		},
+	};
+
+	return (
+		<div>
+			<DataTable
+				columns={columns}
+				data={employees}
+				title="Empleados"
+				pagination
+				paginationComponentOptions={paginationOptions}
+				fixedHeader
+				fixedHeaderScrollHeight="500px"
+				onRowClicked={(row) => console.log(row)}
+				customStyles={customStyles}
+			></DataTable>
+			<h1>red</h1>
+		</div>
+	);
+}
+
+export default CustomDatatable;
